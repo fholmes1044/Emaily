0115695fe4261cfd32207c7d321feab29cea95ab
+ <+>
2023-09-21 13:56:58 -0400

    removed port

--- server/index.js
+++ server/index.js
@@ -2,12 +2,6 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT || 3030;
-
-app.listen(PORT, () => {
-    console.log(`server started on port ${PORT}`);
-  });
-
 app.get('/', (req, res)=> {
     res.send({ hi: 'there'})
 });
